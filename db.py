85453b0bdb1b8f75bdc933b04444492b35a2b1fc
deepak <deepak.muralidharan8@gmail.com>
2019-05-17 19:46:22 +0530

    ipython import partially completed

--- db.py
+++ db.py
@@ -1,9 +1,24 @@
 import mysql.connector
+import pandas as pd
+def prune(syn):
+    x=[]
+    #print(syn)
+    for i in range(2, len(syn) - 3):
+        x.append(syn[i])
+
+    return "".join(x)
+
 conn = mysql.connector.connect(
          user='foouser',
          password='F88Pa%%**',
          host='134.209.144.239',
-         database='stocksdb')
-print(conn)
+         database='stocksdb'
+         )
+#print(conn)
 cur = conn.cursor()
-cur.execute('SELECT * FROM interview')
+cur.execute("SHOW TABLES")
+tables = cur.fetchall()
+for t in tables:
+    t=prune(str(t))
+    df=pd.read_sql_query("select * from "+t,conn)
+    print(df)
