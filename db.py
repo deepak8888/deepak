0a5f1ef4ea06aeee1d9835f955827200846fa635
deepak <deepak.muralidharan8@gmail.com>
2019-05-19 11:38:31 +0530

    task2 completed

--- db.py
+++ db.py
@@ -1,5 +1,11 @@
 import mysql.connector
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt   #Data visualisation libraries
+import seaborn as sns
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+
 def prune(syn):
     x=[]
     #print(syn)
@@ -22,3 +28,14 @@ for t in tables:
     t=prune(str(t))
     df=pd.read_sql_query("select * from "+t,conn)
     print(df)
+df.head()
+df.info()
+df.describe()
+df.columns
+X =df[['volume']]
+y=df[['open','high','low','close']]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=101)
+lm = LinearRegression()
+lm.fit(X_train,y_train)
+predictions = lm.predict(X_test)
+print(predictions)
